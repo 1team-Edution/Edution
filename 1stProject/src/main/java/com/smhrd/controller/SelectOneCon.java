07e707618eedbb03425eb9ef2653d716341e7f1e
Administrator <Administrator@DESKTOP-5PNK83V>
2022-09-28 17:55:11 +0900

    SelectOneCon 리턴 수정 전 - 지현

--- 1stProject/src/main/java/com/smhrd/controller/SelectOneCon.java
+++ 1stProject/src/main/java/com/smhrd/controller/SelectOneCon.java
@@ -11,13 +11,14 @@ public class SelectOneCon implements Controller {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		
-			String note_seq = request.getParameter("note_seq"); // 간혹가다 다른 서버와 통칭할 때 _ 들어있으면 인식을 잘 못하는 경우 있음 주의!
+			String note_seq = request.getParameter("note_seq");
 		
 			NoteDAO n_dao = new NoteDAO();
-			NoteDTO note = n_dao.selectOne(note_seq); // 수집한 파라미터를 넘겨줘야! 그 데이터를 가지고 select구문 실행 가능!! 매개변수로 넘겨주세요!!
+			NoteDTO note = n_dao.selectOne(note_seq);
 			
-			request.setAttribute("note", note); // 객체바인딩!
+			request.setAttribute("note", note);
 			
+			// 여기는 아직 정확하지 않습니다--------
 			return "NoteContent";
 		
 	}
