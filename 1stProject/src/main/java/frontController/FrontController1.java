c0500141ae1d813832bf4396e07db2f2575625ed
smhrd <smhrd@211.223.106.173>
2022-09-28 16:01:47 +0900

    Controller수정 index, loginpage 연결까지 - 주희

---
+++ 1stProject/src/main/java/frontController/FrontController1.java
@@ -0,0 +1,72 @@
+package frontController;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.smhrd.controller.Controller;
+import com.smhrd.controller.LoginPage;
+import com.smhrd.controller.indexView;
+
+@WebServlet("*.do")
+public class FrontController1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	Map<String, Controller> mappings;
+	
+	public void init(ServletConfig config) throws ServletException {
+
+		mappings = new HashMap<>();
+		
+		mappings.put("/indexView.do", new indexView());
+		mappings.put("/LoginPage.do", new LoginPage());
+		
+		
+	}
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String url=request.getRequestURI();
+		
+
+		String cpath=request.getContextPath();
+
+		String command = url.substring( cpath.length() ); 
+
+		
+		String nextView=null;
+		
+		Controller con = null;
+		
+		// 요청을 구분해서 각각의 기능을 실행
+		// HashMap 안에 있는 pojo 꺼내기
+		// HashMap.get(Key);
+		con = mappings.get(command);
+	
+		nextView=con.execute(request, response);
+		
+
+		if(nextView.contains("redirect:/")) {
+
+		
+			response.sendRedirect(nextView.split(":/")[1]);
+		}else {
+			
+
+			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/"+nextView+".jsp");		
+			rd.forward(request, response);
+		}
+		
+		
+		
+	}
+
+}
