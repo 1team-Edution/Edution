eb216e53222d21d02c3a16d8f00235acaff29a08
Administrator <Administrator@DESKTOP-5PNK83V>
2022-09-28 17:53:45 +0900

    InsertCon return 수정 전 - 지현

---
+++ 1stProject/src/main/java/com/smhrd/controller/InsertCon.java
@@ -0,0 +1,40 @@
+package com.smhrd.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.smhrd.model.NoteDAO;
+import com.smhrd.model.NoteDTO;
+
+public class InsertCon implements Controller {
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		String note_content = request.getParameter("note_content");
+		String note_date = request.getParameter("note_date");
+		String note_id = request.getParameter("note_id");
+		String note_tag = request.getParameter("note_tag");
+		String note_title = request.getParameter("note_title");
+		String user_id = request.getParameter("user_id");
+		
+		// 2. DAO로 보내줄 데이터 하나로 묶기 - 생성자 없이 setter 메소드 활용해서 만들어주기!
+		NoteDTO note = new NoteDTO();
+		note.setNote_content(note_content);
+		note.setNote_date(note_date);
+		note.setNote_id(note_id);
+		note.setNote_tag(note_tag);
+		note.setNote_title(note_title);
+		note.setUser_id(user_id);
+
+				NoteDAO n_dao = new NoteDAO();
+		int row = n_dao.insert(note);
+
+		
+		// 여기는 아직 수정 전입니다--------------------------
+		if (row > 0) {
+
+			return "redirect:/SelectAll.do"; 
+
+		} else {
+			return "redirect:/Insert.do";
+		}
+	}
+}
