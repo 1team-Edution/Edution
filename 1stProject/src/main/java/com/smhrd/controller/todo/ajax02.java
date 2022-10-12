5fb9b73f23e5cb10f0f4b7344f4ad40cf70b18c4
smhrd <smhrd@218.157.19.65>
2022-10-12 09:55:13 +0900

    todolist 최종-소명

--- 1stProject/src/main/java/com/smhrd/controller/todo/ajax02.java
+++ 1stProject/src/main/java/com/smhrd/controller/todo/ajax02.java
@@ -2,7 +2,6 @@ package com.smhrd.controller.todo;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,22 +11,20 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
-import com.smhrd.model.TodoDTO;
 import com.smhrd.model.UserDTO;
 
-@WebServlet("/ajax01")
-public class ajax01 extends HttpServlet {
+@WebServlet("/ajax02")
+public class ajax02 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		HttpSession session = request.getSession();
-		
-		ArrayList<TodoDTO> todolist = (ArrayList<TodoDTO>) session.getAttribute("todolist");
+		UserDTO userDto = (UserDTO) session.getAttribute("loginUser");
 		
 		Gson gson = new Gson();
-		String jsonArr = gson.toJson(todolist);
+		String jsonArr = gson.toJson(userDto);
 		
 		response.setContentType("text/html; charset=UTF-8"); 
 		PrintWriter out;
@@ -37,7 +34,7 @@ public class ajax01 extends HttpServlet {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}	
-	//	session.removeAttribute("todolist");
+
 	}
 
 }
